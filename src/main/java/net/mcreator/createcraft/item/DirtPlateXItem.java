49e304357797cd279634b80f6067cc2302bcc858
abnormalhare <abnor@DESKTOP-TLCSQJV>
2020-11-12 17:56:27 -0500

    Fixed DirtFrame

--- src/main/java/net/mcreator/createcraft/item/DirtPlateXItem.java
+++ src/main/java/net/mcreator/createcraft/item/DirtPlateXItem.java
@@ -1,12 +1,25 @@
 
 package net.mcreator.createcraft.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.createcraft.itemgroup.CreateCraftItemGroup;
+import net.mcreator.createcraft.CreatecraftModElements;
+
+import java.util.List;
+
 @CreatecraftModElements.ModElement.Tag
 public class DirtPlateXItem extends CreatecraftModElements.ModElement {
-
 	@ObjectHolder("createcraft:dirt_plate_x")
 	public static final Item block = null;
-
 	public DirtPlateXItem(CreatecraftModElements instance) {
 		super(instance, 38);
 	}
@@ -15,9 +28,7 @@ public class DirtPlateXItem extends CreatecraftModElements.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
 	public static class ItemCustom extends Item {
-
 		public ItemCustom() {
 			super(new Item.Properties().group(CreateCraftItemGroup.tab).maxStackSize(64));
 			setRegistryName("dirt_plate_x");
@@ -43,7 +54,5 @@ public class DirtPlateXItem extends CreatecraftModElements.ModElement {
 			super.addInformation(itemstack, world, list, flag);
 			list.add(new StringTextComponent("A lime dirt plate."));
 		}
-
 	}
-
 }
